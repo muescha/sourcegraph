3a860b5e781632e6b733dceb61ac5f344b031518
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2022-11-09 13:37:34 +0000

    navigator.userAgentData.platform != navigator.platform on Mac

--- client/web/src/components/KeyboardShortcutsHelp/KeyboardShortcutsHelp.tsx
+++ client/web/src/components/KeyboardShortcutsHelp/KeyboardShortcutsHelp.tsx
@@ -104,7 +104,8 @@ export function plaintextKeybindings(keybindings: Keybinding[]): string {
     return keybindings
         .map<string>(keybinding => {
             const ordered = keybinding.ordered.map(key => key.toUpperCase())
-            const joinString = (navigator?.userAgentData?.platform || navigator?.platform).includes('Mac') ? '' : '+'
+            const joinString =
+                navigator?.userAgentData?.platform?.includes('macOS') || navigator?.platform?.includes('Mac') ? '' : '+'
             return [...(keybinding.held || []), ...ordered].map(key => renderShortcutKey(key)).join(joinString)
         })
         .join(' or ')
